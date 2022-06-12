1b1dd956b8c2e1f54dc3794fa8f9c5ee8a1f4613
47kaktus47 <85338017+47kaktus47@users.noreply.github.com>
2022-06-12 15:20:17 +0700

    Update start.py

--- flaskapp/start.py
+++ flaskapp/start.py
@@ -32,19 +32,16 @@ class NetForm(FlaskForm):
  # валидатор проверяет введение данных после нажатия кнопки submit
  # и указывает пользователю ввести данные если они не введены
  # или неверны
-    cho = RadioField('what to do', coerce=int, choices=[(0, 'umnozh'),(1, 'obrat'),(2,'umn na kf')])
-    kfr=StringField('kf for r', validators = [DataRequired()])
-    kfg=StringField('kf for g', validators = [DataRequired()])
-    kfb=StringField('kf for b', validators = [DataRequired()])
+    
+    cho=StringField('rotate', validators = [DataRequired()])
+  
  # поле загрузки файла
  # здесь валидатор укажет ввести правильные файлы
     upload = FileField('Load image', validators=[
     FileRequired(),
     FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
 
-    upload2 = FileField('Load image2', validators=[
-    FileRequired(),
-    FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+
  # поле формы с capture
     recaptcha = RecaptchaField()
  #кнопка submit, для пользователя отображена как send
@@ -61,16 +58,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 ## функция для оброботки изображения 
-def draw(filename1,filename2,cho,kfr,kfg,kfb):
+def draw(filename1,cho):
  ##открываем изображение 
  print(filename1)
  cho=int(cho)
- kfr=int(kfr)
- kfg=int(kfg)
- kfb=int(kfb)
-
  img= Image.open(filename1)
- img1=Image.open(filename2)
+
  
 ##делаем график
  fig = plt.figure(figsize=(6, 4))
@@ -85,28 +78,18 @@ def draw(filename1,filename2,cho,kfr,kfg,kfb):
  plt.savefig(gr_path)
  plt.close()
 
-
- height = 224
- width = 224
- img= np.array(img.resize((height,width)))/255.0
- from numpy.linalg import inv
- height = 224
- width = 224
- img1= np.array(img1.resize((height,width)))/255.0
- if cho==0:
-    img[:,:,0]=np.dot(img[:,:,0],img1[:,:,0])
-    img[:,:,1]=np.dot(img[:,:,1],img1[:,:,1])
-    img[:,:,2]=np.dot(img[:,:,2],img1[:,:,2])
- if cho==1:
-    img[:,:,0]=inv(img[:,:,0])
-    img[:,:,1]=inv(img[:,:,1])
-    img[:,:,2]=inv(img[:,:,2])
- if cho==2:
-    img[:,:,0]=img[:,:,0]*kfr
-    img[:,:,1]=img[:,:,1]*kfg
-    img[:,:,2]=img[:,:,2]*kfb
+ img= Image.open(filename1)
+ for i in range(0,cho):
+    a = img.crop((0, 0, int( y * 0.5), int(x * 0.5)))
+    b = img.crop((int( y * 0.5), 0, y, int(x * 0.5)))
+    c = img.crop((int( y * 0.5), int(x * 0.5), y, x))
+    d = img.crop((0, int(x * 0.5), int( y * 0.5), x))
+    img.paste(a, (0, int(y * 0.5)))
+    img.paste(b, (int(x * 0.5), int(x * 0.5)))
+    img.paste(c, (int(x * 0.5), 0))
+    img.paste(d, (0, 0))
 ##сохраняем новое изображение
- img = Image.fromarray((img * 255).astype(np.uint8))
+
 
  print(img)
  #img = Image.fromarray(img)
@@ -123,7 +106,7 @@ def net():
  form = NetForm()
  # обнуляем переменные передаваемые в форму
  filename1=None
- filename2=None
+
  newfilename=None
  grname=None
  
@@ -131,18 +114,16 @@ def net():
  if form.validate_on_submit():
   # файлы с изображениями читаются из каталога static
   filename1 = os.path.join('./static', secure_filename(form.upload.data.filename))
-  filename2 = os.path.join('./static', secure_filename(form.upload2.data.filename))
-  kfr=form.kfr.data
-  kfg=form.kfg.data
-  kfb=form.kfb.data
+
+
   cho=form.cho.data
   form.upload.data.save(filename1)
-  form.upload2.data.save(filename2)
-  newfilename, grname = draw(filename1,filename2,cho,kfr,kfg,kfb)
+
+  newfilename, grname = draw(filename1,cho)
  # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
  # сети если был нажат сабмит, либо передадим falsy значения
  
- return render_template('net.html',form=form,image1_name=newfilename,gr_name=grname, ishd1=filename1, ishd2=filename2)
+ return render_template('net.html',form=form,image1_name=newfilename,gr_name=grname, ishd1=filename1)
 
 if __name__ == "__main__":
  app.run(host='127.0.0.1',port=5000)
